faa6c28d8d1ec3b0d5f011d5f734d51aab7f3979
Mathew Payne <geekmasher@gmail.com>
2022-08-16 14:56:42 +0100

    Add base exception cases

---
+++ tailor-packs/java-poc/unit-tests/exception_throwables/Except.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Test extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		//
+		Exception err = new Exception("Test");
+
+		// Need to track taint from these functions
+		response.getWriter().print("a " + err.getMessage());
+		response.getWriter().print("a " + err.toString());
+
+		// Other case (unsupported right now)
+		response.getWriter().print("a " + err);
+	}
+}
