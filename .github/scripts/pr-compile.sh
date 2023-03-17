b6c37f32b50c2078ba471e79b223d7d91f0b8b03
Mathew Payne <geekmasher@gmail.com>
2023-03-17 12:57:12 +0000

    Add PR compile script to optimise query compile times

---
+++ .github/scripts/pr-compile.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -e
+
+PR_NUMBER=${1}
+LANGUAGE=${2}
+# to stop recompiling all queries if multiple files are modified
+LIBRARY_SCANNED=false
+
+for file in $(gh pr view $PR_NUMBER --json files --jq '.files.[].path'); do
+    # if the file is a query file .ql or .qll
+    if [[ $file == *.ql ]]; then
+        echo "[+] Compiling $file (in $LANGUAGE)"
+        # compile the query
+        gh codeql query compile \
+            --warnings=error \
+            --search-path=./codeql --additional-packs=./codeql \
+            ./$file
+    # if github folder is modified
+    elif [[ $file == $LANGUAGE/github/* ]] && [[ $LIBRARY_SCANNED == false ]]; then
+        echo "[+] Libray changed, compiling all queries in $LANGUAGE"
+        gh codeql query compile \
+            --warnings=error \
+            --search-path=./codeql --additional-packs=./codeql \
+            ./$LANGUAGE/
+    fi
+done
