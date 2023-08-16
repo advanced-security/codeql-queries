a95ac7dad0e7cfa8558e65531585837d1492c9d3
Keith Hoodlet <securingdev@github.com>
2023-08-16 15:59:50 -0400

    Add updated CWE-532 with tests - new dataflow api

---
+++ tests/java-tests/CWE-532/SensitiveInformation.java
@@ -0,0 +1,29 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Base64;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class SensitiveInformation extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        String requestBody = request.getReader().readLine();
+        session.setAttribute("username", requestBody);
+        // Do something with the request body here
+        String attr = (String)session.getAttribute("username");
+        System.out.print("Username is %s" + attr + "\n");
+        System.out.println(attr);
+        String responseBody = Base64.getEncoder().encodeToString(attr.getBytes());
+        // String responseBody = "Encoded username: " + encodedUsername;
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("UTF-8");
+        response.setStatus(HttpServletResponse.SC_OK);
+        PrintWriter out = response.getWriter();
+        out.print(responseBody);
+        out.flush();
+    }
+}
